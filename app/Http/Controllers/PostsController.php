24aa04a04d231e4f1d9bcda73b38bccf63df312c
L0LAD <audrey.denoual@live.fr>
2020-08-08 15:41:59 +0200

    Restriction d'accès pour la créaction de posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,6 +8,10 @@ use Illuminate\Support\Facades\Validator;
 
 class PostsController extends Controller
 {
+  public function __construct() {
+    $this->middleware('auth');
+  }
+
   public function create() {
     return view('posts.create');
   }
