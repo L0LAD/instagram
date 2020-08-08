f349ce6937f2e00a784542ca97112e939515f5f3
L0LAD <audrey.denoual@live.fr>
2020-08-08 13:46:14 +0200

    Création d'un formulaire d'ajout de posts

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,32 +2,40 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Register</div>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <h1>Add New Post</h1>
+      <form class="form-horizontal" method="POST" action="/p" enctype="multipart/form-data">
+        {{ csrf_field() }}
 
-                <div class="card-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
+        <div class="form-group row{{ $errors->has('caption') ? ' has-error' : '' }}">
+          <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
+          <input id="caption" type="text" class="form-control" name="caption" value="{{ old('caption') }}" required autofocus>
 
-                        <div class="form-group row{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+          @if ($errors->has('caption'))
+          <span class="help-block">
+            <strong>{{ $errors->first('caption') }}</strong>
+          </span>
+          @endif
         </div>
+
+        <div class="form-group row{{ $errors->has('caption') ? ' has-error' : '' }}">
+          <label for="image" class="col-md-4 col-form-label">Post Image</label>
+          <input id="image" type="file" class="form-control-file" name="image" value="{{ old('caption') }}" required autofocus>
+
+          @if ($errors->has('image'))
+          <span class="help-block">
+            <strong>{{ $errors->first('image') }}</strong>
+          </span>
+          @endif
+        </div>
+
+        <div class="row pt-4">
+          <button class="btn btn-primary">Add New Post</button>
+        </div>
+
+      </form>
     </div>
+  </div>
 </div>
 @endsection
