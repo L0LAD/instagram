3d4537e151fba773df52f4867290b786f5e423ac
L0LAD <audrey.denoual@live.fr>
2020-08-08 14:57:36 +0200

    Ajout d'un nouveau post valide

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,15 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostsController extends Controller
 {
-    public function create() {
-      return view('posts.create');
+  public function create() {
+    return view('posts.create');
+  }
+
+  public function store(Request $request) {
+    $dataValidator = \Validator::make($request->all(), [
+      'caption' => 'required|max:255',
+      'image' => 'required|image',
+    ]);
+
+    if ($dataValidator->fails()) {
+      return redirect()->back()
+      ->withErrors($dataValidator)
+      ->withInput();
     }
 
-    public function store() {
-      dd(request()->all());
-    }
+    auth()->user()->posts()->create([
+      'caption' => $request['caption'],
+      'image' => $request['image']
+    ]);
+  }
 }
